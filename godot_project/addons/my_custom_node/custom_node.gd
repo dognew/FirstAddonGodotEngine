6a3eaa95267b14887605df4d8f0deab59a72bcd3
Douglas Fiedler <dognew@gmail.com>
2018-05-06 14:30:42 -0300

    Add custom type MyButton and Remove custom type MyButton

--- godot_project/addons/my_custom_node/custom_node.gd
+++ godot_project/addons/my_custom_node/custom_node.gd
@@ -2,9 +2,11 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-    # Initialization of the plugi goes here
-    pass
-    
+	# Initialization of the plugin goes here
+	# Add the new type with a name, a parent type, a script and an icon
+	add_custom_type("MyButton", "Button", preload("button.gd"), preload("icon.png"))
+
 func _exit_tree():
-    # Clean-up of the plugin goes here
-    pass
+	# Clean-up of the plugin goes here
+	# Always remember to remove it from the engine when deactivated
+	remove_custom_type("MyButton")
